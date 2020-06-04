9a775e47921b9fc90db6f3e6f05850322b5d4448
Jinxia Du <jinxiadu@jinxias-mbp.lan>
2020-06-04 19:53:04 -0400

    fix some mess

--- src/test/java/com/ascending/training/repository/RecipeDaoTest.java
+++ src/test/java/com/ascending/training/repository/RecipeDaoTest.java
@@ -1,4 +1,29 @@
 package com.ascending.training.repository;
+import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RecipeDaoTest {
+    private RecipeDao recipeJDBCDao;
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @Before
+    public void setUp() {
+        recipeJDBCDao = new RecipeDao();
+    }
+
+    @After
+    public void tearDown() {
+        recipeJDBCDao = null;
+    }
+
+    @Test
+    public void getRecipesTest() {
+        logger.debug("start unit test for getRecipesTest...");
+        assertEquals(0, recipeJDBCDao.getRecipes().size());
+    }
 }
