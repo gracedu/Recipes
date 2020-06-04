4fae9a3ae17da9dc35e6f77c80ffd8ce56c9671d
Jinxia Du <jinxiadu@jinxias-mbp.lan>
2020-06-04 10:05:21 -0400

    change the test

--- src/test/java/com/ascending/training/repository/RecipeDaoTest.java
+++ src/test/java/com/ascending/training/repository/RecipeDaoTest.java
@@ -1,14 +1,31 @@
 package com.ascending.training.repository;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class RecipeDaoTest {
+    private RecipeDao recipeJDBCDao;
+    private Logger logger = LoggerFactory.getLogger(getClass());
+    @Before
+    public void setUp() {
+        recipeJDBCDao = new RecipeDao();
+    }
+
+    @After
+    public void tearDown() {
+        recipeJDBCDao = null;
+    }
+
     @Test
     public void getRecipesTest() {
-       // logger.debug("start unit test for getRecipesTest...");
-        RecipeDao recipeJDBCDao = new RecipeDao();
+       logger.debug("start unit test for getRecipesTest...");
+
         assertEquals(0, recipeJDBCDao.getRecipes().size());
         //System.out.println(recipeJDBCDao.getRecipes());
-        assertEquals(6, recipeJDBCDao.getRecipes().size());
+        //assertEquals(6, recipeJDBCDao.getRecipes().size());
     }
 }
