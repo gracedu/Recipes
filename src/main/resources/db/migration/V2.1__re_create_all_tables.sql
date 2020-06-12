bebd6732171063a2aab63c542f69fa04fa9e99a4
Jinxia Du <jinxiadu@jinxias-mbp.lan>
2020-06-12 19:20:46 -0400

    update hibernate

---
+++ src/main/resources/db/migration/V2.1__re_create_all_tables.sql
@@ -0,0 +1,30 @@
+CREATE TABLE recipes (
+	id BIGSERIAL PRIMARY KEY,
+	name varchar NOT NULL,
+	ingredient varchar NOT NULL,
+	description varchar NOT NULL,
+	user_id BIGSERIAL NOT NULL,
+	cuisine varchar NOT NULL,
+	creation_date date default CURRENT_DATE
+);
+
+CREATE TABLE users (
+	id BIGSERIAL PRIMARY KEY,
+	name varchar NOT NULL UNIQUE,
+	email varchar NOT NULL,
+	password varchar NOT NULL
+);
+
+ALTER TABLE recipes ADD CONSTRAINT recipe_user_fk FOREIGN KEY (user_id) REFERENCES users(id);
+
+CREATE TABLE comments (
+    id BIGSERIAL PRIMARY KEY,
+	content varchar NOT NULL,
+	comment_date date default CURRENT_DATE,
+	user_id BIGSERIAL NOT NULL,
+	recipe_id BIGSERIAL NOT NULL
+);
+
+ALTER TABLE comments ADD CONSTRAINT comment_user_fk FOREIGN KEY (user_id) REFERENCES users(id);
+
+ALTER TABLE comments ADD CONSTRAINT comment_recipe_fk FOREIGN KEY (recipe_id) REFERENCES recipes(id);
