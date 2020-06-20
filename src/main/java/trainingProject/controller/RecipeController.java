a3d19b00000b272a4247418fdd41a4829c038f6d
Jinxia Du <jinxiadu@jinxias-mbp.lan>
2020-06-20 16:57:36 -0400

    work on spring

---
+++ src/main/java/trainingProject/controller/RecipeController.java
@@ -0,0 +1,7 @@
+package trainingProject.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class RecipeController {
+}
---
+++ src/main/java/trainingProject/controller/UserController.java
@@ -0,0 +1,28 @@
+package trainingProject.controller;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+import trainingProject.model.User;
+import trainingProject.service.UserService;
+
+import java.util.List;
+
+@RestController
+
+public class UserController {
+
+    private Logger logger = LoggerFactory.getLogger(getClass());
+
+    @RequestMapping(value = "/user", method = RequestMethod.GET)
+    public void getUsers() {
+        logger.debug("hahaha");
+    }
+
+    @RequestMapping(value = "/user/{Id}", method = RequestMethod.GET)
+    public void getUserById(@PathVariable(name="Id") Long id) {
+        logger.debug("the user id is" + id);
+    }
+
+}
